a726f63bd312bc2633dd957be2e1d0f4deb631f8
VICKYGUPTA18 <96422912+VICKYGUPTA18@users.noreply.github.com>
2022-02-22 22:31:01 +0530

    dc

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import "../styles/App.css";
-import Heading from "./components/Heading";
-import InputQuery from "./components/InputQuery";
-import SubHeading from "./components/SubHeading";
-import SubmitButton from "./components/SubmitButton";
+import Heading from "../components/Heading";
+import InputQuery from "../components/InputQuery";
+import SubHeading from "../components/SubHeading";
+import SubmitButton from "../components/SubmitButton";
 
-function App() {
+const App = () => {
   return (
     <div id="main">
       <Heading />
-      <InputQuery />
       <SubHeading />
+      <InputQuery />
+
       <SubmitButton />
     </div>
   );
-}
+};
 export default App;
