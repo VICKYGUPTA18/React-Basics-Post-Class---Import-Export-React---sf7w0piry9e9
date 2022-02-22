1ef779d263c7786cf36bb071de5cf6ae9d05c989
VICKYGUPTA18 <96422912+VICKYGUPTA18@users.noreply.github.com>
2022-02-22 22:07:30 +0530

    ch

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+import Heading from "./components/Heading";
+import InputQuery from "./components/InputQuery";
+import SubHeading from "./components/SubHeading";
+import SubmitButton from "./components/SubmitButton";
 
-const App = () => {
+function App() {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <InputQuery />
+      <SubHeading />
+      <SubmitButton />
     </div>
-  )
+  );
 }
-
-
+export default App;
